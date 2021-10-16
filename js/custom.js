ce9f1e00f8957fc34184e87f99e0db0a37aa19e7
Nicole Hite <nhite@uncc.edu>
2021-10-15 20:26:00 -0400

    Fix for plus sign

--- js/custom.js
+++ js/custom.js
@@ -75,7 +75,7 @@
                             },
                             complete: function() {
                                 $this.text(this.countNum);
-                                $this.text($this.text() + "+"); 
+                                $this.text($this.text() + "aaaa"); 
 
                                 //if($this.attr('plus') == 'True') $this.text(this.countNum + "+");
                                 //alert('finished');
